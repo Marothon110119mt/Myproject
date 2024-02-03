308eeddffdd88e5f2974b71b3eb32d2f4aec2e21
Marothon110119mt <marothon1101@gmail.com>
2024-02-03 09:28:30 -0500

    First Commit
    Date : 03/Feb/2024

---
+++ resources/views/sb-admin/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-gray" style="height: 50px;line-height: 50px;">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Marothon</span>
+          </div>
+        </div>
+</footer>
